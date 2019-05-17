2f7e1b2467798980dc48268537592c034197be3f
2424693824ls <2424693824@qq.com>
2019-05-17 19:38:13 +0800

    得得

--- src/main/java/com/yucong/specialconfig/SpecialConfig.java
+++ src/main/java/com/yucong/specialconfig/SpecialConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Import;
 @Configuration
 public class SpecialConfig {
 
+    @Configuration
     @Import({ConfigAnimal.CreateCat.class, ConfigAnimal.CreateDog.class})
     protected static class BatchImportConfigToCreateBean {
     }
